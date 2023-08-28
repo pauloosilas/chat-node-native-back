fea428478031bb41ac20310d62706825684c1df2
pauloosilas <paulo3356@gmail.com>
2023-08-28 12:52:37 -0300

    send message image

--- controllers/chat_message.js
+++ controllers/chat_message.js
@@ -1,5 +1,6 @@
 import { ChatMessage } from "../models/index.js";
 import { io } from "../utils/index.js";
+import { getFilePath } from "../utils/index.js";
 //send messsage
 const send = (req, res) => {
   const { chat_id, message } = req.body;
@@ -24,6 +25,30 @@ const send = (req, res) => {
   });
 };
 
+const sendImage = (req, res) => {
+  const { chat_id } = req.body;
+  const { user_id } = req.user;
+
+  const chat_message = new ChatMessage({
+    chat: chat_id,
+    user: user_id,
+    message: getFilePath(req.files.image),
+    type: "IMAGE",
+  });
+
+  chat_message.save(async (error) => {
+    if (error) {
+      res.status(400).send({ msg: "Error ao enviar a imagem" });
+    } else {
+      const data = await chat_message.populate("user");
+      io.sockets.in(chat_id).emit("message", data); //emit para todos os usuarios data
+      io.sockets.in(`${chat_id}_notify`).emit("message_notify", data);
+      res.status(400).send({});
+    }
+  });
+};
+
 export const ChatMessageController = {
   send,
+  sendImage,
 };
--- routes/chat_message.js
+++ routes/chat_message.js
@@ -7,5 +7,10 @@ const mdUpload = multiparty({ uploadDir: "./uploads/images" });
 const api = express.Router();
 
 api.post("/chat/message", [mdAuth.asureAuth], ChatMessageController.send);
+api.post(
+  "/chat/message/image",
+  [mdAuth.asureAuth, mdUpload],
+  ChatMessageController.sendImage
+);
 
 export const chatMessageRoutes = api;
